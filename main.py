f176b1b38351163950dff4f235346ba462c134c6
Rathnavel-Rat <rathnavel8026@gmail.com>
2021-04-17 18:38:15 +0530

    first

---
+++ main.py
@@ -0,0 +1,28 @@
+from flask import Flask,render_template,request,Response,session,flash,redirect,url_for,flash
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import playsound 
+from flask import jsonify
+import re,os
+import json
+from werkzeug.utils import secure_filename
+from playsound import playsound
+app=Flask(__name__)
+app.config['MYSQL_HOST']="localhost"
+app.config['MYSQL_USER']="root"
+app.config['MYSQL_PASSWORD']=""
+app.config['MYSQL_DB']="agro"
+app.secret_key = 'the random string'
+app.config['secret_key']="dhfebfhuiu34h3u7rh387fh8723h7hr83h27h8"
+
+mysql=MySQL(app)
+
+
+@app.route("/")
+def home():
+    return render_template("login.html")
+
+
+ 
+if __name__ == '__main__':
+    app.run(debug=True)
