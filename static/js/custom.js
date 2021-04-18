e83bb8d3ca8e8ee9d4a7d47728b42bb9e238bf86
Rathnavel-Rat <rathnavel8026@gmail.com>
2021-04-18 19:51:31 +0530

    Rental

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,8 +1,8 @@
 (function($) {
     "use strict";
-	
+
 	/* ..............................................
-	   Loader 
+	   Loader
 	   ................................................. */
 	$(window).on('load', function() {
 		$('.preloader').fadeOut();
@@ -211,95 +211,7 @@
 	$(".brand-box").niceScroll({
 		cursorcolor: "#9b9b9c",
 	});
-	
-	
+
+
 }(jQuery));
 
-
-//
-// Clinic Photo Gallery
-// - Stop Autoplay
-// --------------------------------------------------
-$('#clinicPhotoGallery').carousel({
-    interval: false
-})
-
-
-
-// Show/Hide Thumbnail Gallery
-// ---------------------------------------
-$(document).ready(function(){
-  // Show Thumbnail Gallery on Click
-  $("#showThumbnailGallery").click(function(){
-    $(".thumbnails-carousel").removeClass("hide");
-    $("#showThumbnailGallery").addClass("hide");
-    $("#hideThumbnailGallery").removeClass("hide");
-  });
-  // Hide Thumbnail Gallery on Click
-  $("#hideThumbnailGallery").click(function(){
-    $(".thumbnails-carousel").addClass("hide");
-    $("#showThumbnailGallery").removeClass("hide");
-    $("#hideThumbnailGallery").addClass("hide");
-  });
-});
-
-
-// Photo Gallery Thumbnail
-// thumbnails.carousel.js jQuery plugin
-// ---------------------------------------
-;(function(window, $, undefined) {
-
-	var conf = {
-		center: true,
-		backgroundControl: false
-	};
-
-	var cache = {
-		$carouselContainer: $('.thumbnails-carousel').parent(),
-		$thumbnailsLi: $('.thumbnails-carousel li'),
-		$controls: $('.thumbnails-carousel').parent().find('.carousel-control')
-	};
-
-	function init() {
-		cache.$carouselContainer.find('ol.carousel-indicators').addClass('indicators-fix');
-		cache.$thumbnailsLi.first().addClass('active-thumbnail');
-
-		if(!conf.backgroundControl) {
-			cache.$carouselContainer.find('.carousel-control').addClass('controls-background-reset');
-		}
-		else {
-			cache.$controls.height(cache.$carouselContainer.find('.carousel-inner').height());
-		}
-
-		if(conf.center) {
-			cache.$thumbnailsLi.wrapAll("<div class='center clearfix'></div>");
-		}
-	}
-
-	function refreshOpacities(domEl) {
-		cache.$thumbnailsLi.removeClass('active-thumbnail');
-		cache.$thumbnailsLi.eq($(domEl).index()).addClass('active-thumbnail');
-	}
-
-	function bindUiActions() {
-		cache.$carouselContainer.on('slide.bs.carousel', function(e) {
-  			refreshOpacities(e.relatedTarget);
-		});
-
-		cache.$thumbnailsLi.click(function(){
-			cache.$carouselContainer.carousel($(this).index());
-		});
-	}
-
-	$.fn.thumbnailsCarousel = function(options) {
-		conf = $.extend(conf, options);
-
-		init();
-		bindUiActions();
-
-		return this;
-	}
-
-})(window, jQuery);
-
-$('.thumbnails-carousel').thumbnailsCarousel();
