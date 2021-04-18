f26e8e63f45f24480807162230fddf19833e7f2a
Rathnavel-Rat <rathnavel8026@gmail.com>
2021-04-18 11:10:38 +0530

    new

--- main.py
+++ main.py
@@ -15,9 +15,65 @@ app.config['secret_key']="dhfebfhuiu34h3u7rh387fh8723h7hr83h27h8"
 mysql=MySQL(app)
 
 
-@app.route("/")
+@app.route('/')
 def home():
-    return render_template("login.html")
+     return render_template('login.html') 
+
+@app.route('/', methods =['GET', 'POST']) 
+def login():
+    msg = '' 
+    print(request.method)
+    if request.method == 'POST': 
+        emailaddress = request.form['emailaddress'] 
+        password = request.form['password'] 
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor) 
+        cursor.execute('SELECT * FROM register WHERE emailaddress = % s AND password = % s', (emailaddress, password)) 
+        register = cursor.fetchone()
+        print(emailaddress)
+        if register: 
+            session['loggedin'] = True
+            session['emailaddress'] = register["emailaddress"] 
+            return render_template('home.html')
+        else: 
+            msg = 'Incorrect emailaddress / password !'
+            return render_template('login.html',msg = msg)
+
+@app.route('/index',methods = ['POST', 'GET'])
+def index():
+     
+    return render_template('index.html',msg=session["emailaddress"])           
+
+@app.route('/register',methods = ['POST', 'GET'])
+def register():
+    msg = ''
+    print(request)
+    if request.method == 'POST'and 'password' in request.form and 'emailaddress' in request.form:
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
+        emailaddress = request.form['emailaddress']
+        phoneno = request.form['phoneno']
+        password = request.form['password']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor) 
+        cursor.execute('SELECT * FROM register WHERE emailaddress = % s AND password = % s', (emailaddress, password)) 
+        registers = cursor.fetchone()
+        if registers:
+            msg = 'Account already exists !'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', emailaddress):
+            msg = 'Invalid email address !'
+        elif not re.match(r'[A-Za-z0-9]+', emailaddress):
+            msg = 'Username must contain only characters and numbers !'
+        elif not emailaddress or not password :
+            msg = 'Please fill out the form !'
+        else:
+            cur=mysql.connection.cursor()
+            cur.execute("INSERT INTO register (firstname,lastname,emailaddress,phoneno,password) VALUES (%s,%s,%s,%s,%s)",(firstname,lastname,emailaddress,phoneno,password))
+            mysql.connection.commit()
+            cur.close()
+            msg = 'You have successfully registered !'
+    elif request.method == 'POST':
+        msg = 'Please fill out the form !'
+    return render_template('register.html', msg = msg)
+
 
 
  
